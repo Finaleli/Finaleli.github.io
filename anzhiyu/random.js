b0520075e7abad157d4c8a72591a96dff2a131d9
Finalelk <65846642+Finalelk@users.noreply.github.com>
2023-03-12 22:45:51 +0800

    Site updated: 2023-03-12 22:45:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/9ff79b23.html","posts/551f943f.html","posts/0.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/9ff79b23.html","posts/0.html","posts/551f943f.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
