4dfcd9b871e6ba5753ce9e98b69ce88a01626471
Finaleli <116935067+Finaleli@users.noreply.github.com>
2023-05-10 09:45:25 +0000

    Publish Post source/_posts/123.md by Qexo Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/9ff79b23.html","posts/c81ea237.html","posts/f40d8d1a.html","posts/551f943f.html","posts/0.html","posts/7f7d6eaa.html"];function toRandomPost(){pjax.loadUrl("/"+posts[Math.floor(Math.random()*posts.length)])}
+var posts=["posts/884863d2.html","posts/9ff79b23.html","posts/c81ea237.html","posts/f40d8d1a.html","posts/551f943f.html","posts/0.html","posts/7f7d6eaa.html"];function toRandomPost(){pjax.loadUrl("/"+posts[Math.floor(Math.random()*posts.length)])}
